a514a360d70f651db9ead8ed901415acbd458f70
Will Feng (DPER) <willfeng@fb.com>
2021-11-22 15:45:19 -0800

    update

--- train_vit_dummy_data.py
+++ train_vit_dummy_data.py
@@ -273,7 +273,7 @@ def main():
         else:
             if args.local_rank == 0:
                 print("Using native Torch DistributedDataParallel.")
-            model = NativeDDP(model, device_ids=[args.local_rank])
+            model = NativeDDP(model, device_ids=[args.local_rank], find_unused_parameters=True)
         # NOTE: EMA model does not need to be wrapped by DDP
 
     start_epoch = 0
