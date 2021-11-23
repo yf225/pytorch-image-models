e60629e2cb53e31ab9a99993d126e763977c77c2
Will Feng (DPER) <willfeng@fb.com>
2021-11-22 16:07:27 -0800

    update

--- train_vit_dummy_data.py
+++ train_vit_dummy_data.py
@@ -265,6 +265,7 @@ def main():
         if args.local_rank == 0:
             print('Using native Torch AMP. Training in mixed precision.')
     else:
+        loss_scaler = NativeScaler()
         if args.local_rank == 0:
             print('AMP not enabled.')
 
