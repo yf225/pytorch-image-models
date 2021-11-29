ceab90a64c5da985079db12da04bc51a254c9dbe
Will Feng (DPER) <willfeng@fb.com>
2021-11-29 14:11:54 -0800

    update

--- train_vit_pt_timm_gpu.py
+++ train_vit_pt_timm_gpu.py
@@ -8,8 +8,8 @@ rm -rf ./pytorch-image-models || true
 git clone https://github.com/yf225/pytorch-image-models.git -b vit_dummy_data
 cd pytorch-image-models && git pull
 
-python3 train_vit_pt_timm_gpu_graph.py --apex-amp --apex-amp-opt-level=O3 --mode=eager --micro_batch_size=2
-python3 train_vit_pt_timm_gpu_graph.py --native-amp --mode=graph --micro_batch_size=2
+python3 train_vit_pt_timm_gpu.py --apex-amp --apex-amp-opt-level=O3 --mode=eager --micro_batch_size=2
+python3 train_vit_pt_timm_gpu.py --native-amp --mode=graph --micro_batch_size=2
 """
 import argparse
 import time
