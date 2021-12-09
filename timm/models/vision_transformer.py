17f92c682271831799bf1de7af31704e8fe6a2b6
Will Feng (DPER) <willfeng@fb.com>
2021-12-09 13:49:47 -0800

    update

--- timm/models/vision_transformer.py
+++ timm/models/vision_transformer.py
@@ -201,6 +201,7 @@ class Attention(nn.Module):
         attn = (q @ k.transpose(-2, -1)) * self.scale
         attn = attn.softmax(dim=-1)
         attn = self.attn_drop(attn)
+
         x = (attn @ v).transpose(1, 2).reshape(B, N, C)
         x = self.proj(x)
         x = self.proj_drop(x)
