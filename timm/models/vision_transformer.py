28f80a649324ba4aa9a30f4733509518969d79ff
Will Feng (DPER) <willfeng@fb.com>
2021-12-09 13:48:20 -0800

    update

--- timm/models/vision_transformer.py
+++ timm/models/vision_transformer.py
@@ -188,24 +188,19 @@ class Attention(nn.Module):
         head_dim = dim // num_heads
         self.scale = head_dim ** -0.5
 
-        # self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
-        self.query_dense = nn.Linear(in_features=dim, out_features=dim, bias=qkv_bias)
-        self.key_dense = nn.Linear(in_features=dim, out_features=dim, bias=qkv_bias)
-        self.value_dense = nn.Linear(in_features=dim, out_features=dim, bias=qkv_bias)
-        self.attn_drop = nn.Dropout(attn_drop) if attn_drop > 0. else nn.Identity()
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
         self.proj = nn.Linear(dim, dim)
-        self.proj_drop = nn.Dropout(proj_drop) if proj_drop > 0. else nn.Identity()
+        self.proj_drop = nn.Dropout(proj_drop)
 
     def forward(self, x):
         B, N, C = x.shape
-        # qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
-        # q, k, v = qkv.unbind(0)   # make torchscript happy (cannot use tensor as tuple)
-        q, k, v = self.query_dense(x), self.key_dense(x), self.value_dense(x)
+        qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        q, k, v = qkv.unbind(0)   # make torchscript happy (cannot use tensor as tuple)
 
         attn = (q @ k.transpose(-2, -1)) * self.scale
         attn = attn.softmax(dim=-1)
         attn = self.attn_drop(attn)
-
         x = (attn @ v).transpose(1, 2).reshape(B, N, C)
         x = self.proj(x)
         x = self.proj_drop(x)
